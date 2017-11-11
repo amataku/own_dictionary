a5bd59fb222f64931c42a74d8f93cb9ad34cfd2c
amataku <amataku13@gmail.com>
2017-11-11 18:25:35 +0900

    add index and new page

--- own_dic/app/controllers/words_controller.rb
+++ own_dic/app/controllers/words_controller.rb
@@ -1,8 +1,23 @@
 class WordsController < ApplicationController
   def index
+    @word = Word.all
   end
 
   def show
     @word = Word.find_by(:alphabet => params[:alphabet])
+  end
+
+  def new
+  end
+
+  def create
+    @word = Word.new
+    @word.name = params[:word][:name]
+    @word.alphabet = params[:word][:alphabet]
+    @word.reading = params[:word][:reading]
+    @word.genre = params[:word][:genre]
+    @word.mean = params[:word][:mean]
+    @word.save
+    redirect_to '/words/index'
   end
 end
--- own_dic/db/migrate/20171111053502_create_words.rb
+++ own_dic/db/migrate/20171111053502_create_words.rb
@@ -2,9 +2,10 @@ class CreateWords < ActiveRecord::Migration[5.1]
   def change
     create_table :words do |t|
       t.string :name
-      t.string :alphabet
       t.string :reading
+      t.string :alphabet
       t.text :mean
+      t.string :genre
 
       t.timestamps
     end
